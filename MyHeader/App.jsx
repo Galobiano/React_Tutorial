4eb12616d07a6d08d35a63a576c24080f080d678
Yur0_0 <151698267+Galobiano@users.noreply.github.com>
2025-01-29 15:42:00 -0800

    Add files via upload

---
+++ MyHeader/App.jsx
@@ -0,0 +1,21 @@
+import Header from './Header/Header.jsx'
+
+
+function App() {
+  
+     return(
+        <>
+          <Header/>
+        </>
+     );
+   
+}
+
+export default App
+
+// props (properties) - shared between component 
+// propsTypes = that ensures that the passed value is of the correct datatype
+// defaultProps = case they are not passed from the parent component 
+
+// conditional rendering - allow to control what gets rendered in your application based on certain condition
+//  (show, hide, or change component)
