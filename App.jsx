1700c7c2a0489a5282568c890b2d70786d391446
Yur0_0 <151698267+Galobiano@users.noreply.github.com>
2025-01-30 05:08:01 -0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import Card from './Card.jsx'
+
+function App() {
+  
+     return(
+        <>
+        <Card/>
+        </>
+     );
+   
+}
+
+export default App
+
+// props (properties) - shared between component 
+// propsTypes = that ensures that the passed value is of the correct datatype
+// defaultProps = case they are not passed from the parent component 
+
+// conditional rendering - allow to control what gets rendered in your application based on certain condition
+//  (show, hide, or change component)
