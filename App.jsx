e18a55d5b41ef297fc713cae00d011e05dc85a45
Yur0_0 <151698267+Galobiano@users.noreply.github.com>
2025-01-30 19:40:50 -0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import ColorPicker from "./ColorPicker";
+
+
+
+
+function App() {
+
+            return(
+              <>
+                
+                  <ColorPicker/>
+
+                </>
+            );   
+
+   
+}
+
+export default App
+
+
+
+
+
+
+
