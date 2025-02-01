dfbc90bca89cac59d29201f6472c9a3f9e10005f
Galobiano <151698267+Galobiano@users.noreply.github.com>
2025-02-01 13:46:53 -0800

    Add another component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
- import MyComponent from './MyComponent.jsx'
+
 
 function App() {
 
             return(
               <>
                
-               <MyComponent/>
+               
                 </>
             );   
 
@@ -13,22 +13,6 @@ function App() {
 }
 
 export default App
-
-// useEffect () = React hook that tell react do some when (pick one):
-//          this component re-renders
-//          this component mounts
-//          the state of value
-
-
-//1. useEffect(() => {})            runs after every re-render
-//2. useEffect(() => {},[])         run only on mount
-//3. useEffect(() => {}, [value])   run on mount + when value changes 
-
-
-
-
-// why used useEffect
-// 1. to keep you code oraganize
-
-// useEffect is like an "automatic updater."
-// useEffect is a React hook that runs side effects in a functional component. 
+// UseContext() = React hook that allows you to share value 
+//              between multiple level of component
+//              without passing props through each level
